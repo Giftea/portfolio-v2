1f115819f235af0a82c997de3afafad60cfc7d91
giftea <eleojogiftuhiene@gmail.com>
2024-01-14 10:37:37 +0100

    Updated preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,8 +1,17 @@
+import { Bars } from "react-loader-spinner";
 
 const Preloader = () => {
   return (
     <div className="st-preloader">
-      <div className="st-preloader-in"></div>
+      <Bars
+        height="80"
+        width="80"
+        color="#b8916e"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 };
